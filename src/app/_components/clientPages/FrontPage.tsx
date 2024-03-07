4e0a8d9323139578311626eb5aece2f8ab8e8d78
Jenni Tran <jenni90232@gmail.com>
2024-03-08 00:14:13 +0200

    add homepage

--- src/app/_components/clientPages/FrontPage.tsx
+++ src/app/_components/clientPages/FrontPage.tsx
@@ -5,24 +5,33 @@ import { Button } from "@nextui-org/react";
 export default function FrontPage() {
   return (
     <main className="mx-auto">
-      <div className="mx-auto container bg-cover bg-center frontpage flex">
-        <div className="relative flex-1 md:flex-3 p-2">
-          <div className=" text-center">
-            <h1>Tervetuloa AIBotHelperiin!</h1>
-            <Button color="primary">Seuraava</Button>
-            <h3>
-              Olen täällä avustamassa sinua luomaan ansioluettelon. Annan
-              vihjeitä ja autan innoillani liittyen IT-alan töihin.
-            </h3>
-            <h4>
-              Tähdellä (*) merkityt kohdat ovat pakollisia täyttää. Voit jättää
-              muut kohdat tyhjiksi, jos niihin ei ole tietoja. Paina
-              &quot;seuraava&quot; etenemiseksi.
-            </h4>
+      <div className="mx-auto container frontpage flex">
+        <div className="p-2 text-center flex-col p-3 mt-10 justify-center">
+          <h1 className="text-4xl font-bold mb-4 ">
+            Tervetuloa AIBotHelperiin!
+          </h1>
 
-            <div>
-              <img src="/images/kirja.jpg" alt="kirja" className=" w-50"></img>
+          <div className="p-4 mt-5 flex items-center">
+            <img
+              src="/images/kirja.jpg"
+              alt="kirja"
+              className=" w-40 h-auto  mr-8 "
+            ></img>
+            <div className="md:w-1/2 text left">
+              <h3 className="mb-2">
+                Olen täällä avustamassa sinua luomaan ansioluettelon. Annan
+                vihjeitä ja autan innoillani liittyen IT-alan töihin.
+              </h3>
+              <h4>
+                Tähdellä (*) merkityt kohdat ovat pakollisia täyttää. Voit
+                jättää muut kohdat tyhjiksi, jos niihin ei ole tietoja. Paina
+                &quot;seuraava&quot; etenemiseksi.
+              </h4>
             </div>
+          </div>
+
+          <div className="flex justify-center mt-10">
+            <Button color="primary">Seuraava</Button>
           </div>
         </div>
       </div>
